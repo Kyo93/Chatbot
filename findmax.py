72f9583ca8e2a810d4cf1d3cd001fc1ca25e8cd5
Nguyen Thiep <KyO@KyO.minhviettransports.com>
2020-03-24 11:09:06 +0700

    add code 24-03

---
+++ findmax.py
@@ -0,0 +1,13 @@
+A = [20, 1, 5, 5, 10, 12, 15, -2, -4, -5, -4]
+Amax = A[0]
+for x in A:
+    if x > Amax:
+        Amax = x
+print("Max là:",Amax)
+A.remove(Amax)
+Amax2 = A[0]
+for y in A:
+    if y > Amax2:
+        Amax2 = y
+# print("Max là:", Amax)
+print("Max2nd là:", Amax2)
--- max2nd-lambda.py
+++ max2nd-lambda.py
@@ -1,4 +1,6 @@
-# tìm số lớn thứ 2 cách 1
+# tìm số lớn thứ 2
+# dùng hàm vô danh:
+# lambda tham số: biểu thức
 find_max2 = lambda a: max(filter(max(a).__ne__, a))
 l = [10, 2, 3, 4, 5, -12, -9, 9]
 print(find_max2(l))
