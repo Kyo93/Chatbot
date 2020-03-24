d6b55922b193ebce270973c0cd76aa2d8c35ef1d
KyO93 <thienphong183@gmail.com>
2020-03-24 16:36:36 +0700

    fibonaci

--- findmax-for.py
+++ findmax-for.py
@@ -3,11 +3,11 @@ Amax = A[0]
 for x in A:
     if x > Amax:
         Amax = x
-print("Max là:",Amax)
+#print("Max là:",Amax)
 A.remove(Amax)
 Amax2 = A[0]
 for y in A:
     if y > Amax2:
         Amax2 = y
-# print("Max là:", Amax)
+print("Max là:", Amax)
 print("Max2nd là:", Amax2)
